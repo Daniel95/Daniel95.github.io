0286b15bd2ea79ce1aa9188dab92fa6fbbedeff5
Daniel95 <idaniel95@me.com>
2016-10-18 18:43:09 +0200

    add portfolio

---
+++ script.js
@@ -0,0 +1,80 @@
+//values
+var dia = [ 1,
+    {
+        img: "slider/voodoo.png",
+        hText: "Voodoo",
+        pText: "3D VR Horror Game",
+        page: "voodoo",
+    },
+    {
+        img: "slider/waterrun.jpg",
+        hText: "",
+        pText: "",
+        page: "waterrun",
+    },
+    {
+        img: "slider/treehousedefense.png",
+        hText: "2D Towerdefense Game",
+        pText: "Aim",
+        page: "treehousedefense",
+    },
+    {
+        img: "slider/meanracers.png",
+        hText: "Meanracers",
+        pText: "2D Online Javascript Game",
+        page: "meanracers",
+    }
+]
+
+$(document).ready(function () {
+    var slideSpeed = 4200;
+    
+    //Functions
+    var nextDia = function() {
+        $("#PreviousDia").css("background-image", "url(images/" + dia[dia[0]].img + ")");
+        $("#PreviousDia h1").text(dia[dia[0]].hText);
+        $("#PreviousDia p").text(dia[dia[0]].pText);
+       
+        if (dia.length-1 == dia[0]++)
+            dia[0] = 1;
+       
+        $("#CurrentDia").css("background-image", "url(images/" + dia[dia[0]].img + ")");
+        $("#CurrentDia h1").text(dia[dia[0]].hText);
+        $("#CurrentDia p").text(dia[dia[0]].pText);
+        
+        $("#Banner").css("margin-left", "0");
+        $("#Banner").animate({"margin-left": "-100%"}, 1300);
+    }
+
+    var diaShow = window.setInterval(nextDia, slideSpeed)
+
+    $("#Banner").css("margin-left", "0");
+    loadStartValuesSlider();
+    
+    //Events
+    $("#Banner").mouseenter(function() {clearInterval(diaShow)});
+    $("#Banner").mouseleave(function() {diaShow = window.setInterval(nextDia, slideSpeed)});
+    
+    
+    $("a").click(function() {
+        if (this.href.search("#") >= 0)
+            loadPage( this.href.substr(this.href.search("#")+1) );
+    });
+    
+    //click function for banner
+    $('body').on('click','section',function(e){
+        loadPage(dia[dia[0]].page);
+    })
+        
+    var loadPage = function(page) {
+        $(".content").load("pages/" + page + ".html");
+    }
+})
+
+function loadStartValuesSlider() {
+    $("#PreviousDia").css("background-image", "url(images/" + dia[dia[0]].img + ")");
+    $("#PreviousDia h1").text(dia[dia[0]].hText);
+    $("#PreviousDia p").text(dia[dia[0]].pText);
+    
+    //dia[0] = Math.random() * dia.length - 2;
+}
