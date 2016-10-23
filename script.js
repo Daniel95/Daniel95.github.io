f24fe2917d8fc7ab80013e9d66724c11454b2683
Daniel95 <idaniel95@me.com>
2016-10-23 19:20:05 +0200

    fixed link to page not working on some pages

--- script.js
+++ script.js
@@ -55,15 +55,21 @@ $(document).ready(function () {
     $("#Banner").mouseenter(function() {clearInterval(diaShow)});
     $("#Banner").mouseleave(function() {diaShow = window.setInterval(nextDia, slideSpeed)});
     
-    
+    /*
     $("a").click(function() {
         if (this.href.search("#") >= 0)
             loadPage( this.href.substr(this.href.search("#")+1) );
-    });
+    });*/
     
     //click function for banner
     $('body').on('click','section',function(e){
         loadPage(dia[dia[0]].page);
+    })
+    
+    //click function for banner
+    $('body').on('click','a',function(e){
+        if (this.href.search("#") >= 0)
+            loadPage( this.href.substr(this.href.search("#")+1) );
     })
         
     var loadPage = function(page) {
